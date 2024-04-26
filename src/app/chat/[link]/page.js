a8a15757f83e9233d47c72a71ccae1996fad2ea0
Ralph Vargas <ralphvargassorm@gmail.com>
2024-04-26 00:08:09 -0400

    chat page

--- src/app/chat/[link]/page.js
+++ src/app/chat/[link]/page.js
@@ -1,31 +1,45 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import styles from "./chat.module.css";
 import { useParams } from "next/navigation";
 import { getAIResponse } from "@/utils/api";
 import QACard from "@/components/QACard/QACard";
+import { Spin, Space, Button, Input } from "antd";
+import { SendOutlined } from "@ant-design/icons";
+
 const Chat = () => {
+  const chatHistoryRef = useRef(null);
+  const inputRef = useRef(null);
+  const { link } = useParams();
   const [question, setQuestion] = useState("");
+  const [disable, setDisable] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [chatHistory, setChatHistory] = useState([
     {
       role: "assistant",
       content:
-        "You are a helpful AI assistant. Answser questions to your best ability.",
+        "You are a helpful AI assistant. Answer questions to your best ability.",
     },
   ]);
-  const { link } = useParams();
 
   const handleSend = async () => {
-    setChatHistory((prev) => [
-      ...prev,
-      {
-        role: "user",
-        content: question,
-      },
-    ]);
+    if (question.trim() === "") {
+      alert("Please enter a question before sending.");
+      return;
+    }
+    const newUserMessage = {
+      role: "user",
+      content: question,
+    };
     setQuestion("");
-    const response = await getAIResponse(link, chatHistory);
+    setChatHistory((prev) => [...prev, newUserMessage]);
+    setDisable(true);
+    setLoading(true);
+    const response = await getAIResponse(link, [
+      ...chatHistory,
+      newUserMessage,
+    ]);
     setChatHistory((prev) => [
       ...prev,
       {
@@ -33,20 +47,54 @@ const Chat = () => {
         content: response,
       },
     ]);
+    setLoading(false);
+    setDisable(false);
   };
 
+  const scrollToBottom = () => {
+    if (chatHistoryRef.current) {
+      chatHistoryRef.current.scrollTop = chatHistoryRef.current.scrollHeight;
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [chatHistory]);
+
+  useEffect(() => {
+    scrollToBottom();
+    inputRef.current.focus();
+  }, []);
+
   return (
-    <div>
-      <h1>Chat</h1>
-      {chatHistory.map((chat, id) => {
-        return <QACard key={id} user={chat.role} content={chat.content} />;
-      })}
-      <input
-        type="text"
-        value={question}
-        onChange={(e) => setQuestion(e.target.value)}
-      />
-      <button onClick={handleSend}>CLick me</button>
+    <div className={styles.container}>
+      <div ref={chatHistoryRef} className={styles.chatHistory}>
+        {chatHistory.map((chat, id) => {
+          return <QACard key={id} user={chat.role} content={chat.content} />;
+        })}
+        {loading ? <Spin /> : <></>}
+      </div>
+      <div className={styles.chatForm}>
+        <Space.Compact
+          style={{
+            alignSelf: "center",
+            justifySelf: "center",
+            width: "100%",
+          }}
+        >
+          <Input
+            ref={inputRef}
+            size="large"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+            placeholder="Message AI Helper"
+            disabled={disable}
+          />
+          <Button size="large" onClick={handleSend} type="primary">
+            <SendOutlined />
+          </Button>
+        </Space.Compact>
+      </div>
     </div>
   );
 };
